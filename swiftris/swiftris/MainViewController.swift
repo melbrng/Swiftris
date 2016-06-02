d9f96a48950f87eb62296e170b4546e77072ea49
Melissa <melbrng@gmail.com>
2016-06-02 10:11:07 -0400

    addition of authenticatePlayer function for logging into gamekit and retrieving the default leaderboard

--- swiftris/swiftris/MainViewController.swift
+++ swiftris/swiftris/MainViewController.swift
@@ -7,35 +7,74 @@
 //
 
 import UIKit
+import GameKit
 
 
 class MainViewController: UIViewController
 {
     
     @IBOutlet weak var gamePlaySegmentControl: UISegmentedControl!
+    var gameCenterEnabled:Bool!
+    var leaderboardIdentifier:String?
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
+        authenticateLocalPlayer()
+        
         gamePlaySegmentControl.selectedSegmentIndex = 0
         
     }
     
-    @IBAction func gamePlaySegmentControl(sender: AnyObject)
-    {
-        //print("index : " + String(gamePlaySegmentControl.selectedSegmentIndex))
-    }
-
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
         if(segue.identifier == "showGameView")
         {
-
+            
             if let gameViewController:GameViewController = segue.destinationViewController as? GameViewController
             {
                 gameViewController.gamePlay = gamePlaySegmentControl.selectedSegmentIndex
             }
         }
+    }
+    
+    func authenticateLocalPlayer()
+    {
+        let localPlayer = GKLocalPlayer.localPlayer()
+        localPlayer.authenticateHandler = {(viewController, error) -> Void in
+            if ((viewController) != nil)
+            {
+                self.presentViewController(viewController!, animated: true, completion: nil)
+            }
+            else
+            {
+                print("(GameCenter) Player authenticated: \(GKLocalPlayer.localPlayer().authenticated)")
+                
+                if (localPlayer.authenticated) {
+                    self.gameCenterEnabled = true
+                    
+                    // Get the default leaderboard identifier
+                    localPlayer.loadDefaultLeaderboardIdentifierWithCompletionHandler({ (leaderboardIdentifier, error) in
+                        if (error != nil)
+                        {
+                            print(error?.localizedDescription)
+                        }
+                        else
+                        {
+                            self.leaderboardIdentifier = leaderboardIdentifier!
+                            print(self.leaderboardIdentifier)
+                        }
+                    });
+                    
+                }
+                    
+                else
+                {
+                    self.gameCenterEnabled = false;
+                }
+            }
+        }
+        
     }
 }
